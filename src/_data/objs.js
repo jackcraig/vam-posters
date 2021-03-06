ddcaaebe576cf8b37b3ab2700eac5f823a2babe9
Jack Craig <jackthfc@gmail.com>
2021-03-06 17:34:39 +0000

    Call API for poster titles

--- src/_data/objs.js
+++ src/_data/objs.js
@@ -11,12 +11,12 @@ const Cache = require('@11ty/eleventy-cache-assets');
 module.exports = async function() {
   try {
     // https://developer.github.com/v3/repos/#get
-    let json = await Cache("https://api.vam.ac.uk/v2/objects/search?id_material=AAT12849&id_place=x28849&response_format=json", {
+    let json = await Cache("https://api.vam.ac.uk/v2/objects/search/?page=1&id_category=THES252692&kw_object_type=Poster&response_format=json", {
       duration: "1d", // 1 day
       type: "json" // also supports "text" or "buffer"
     });
     console.log("hello jack" + json.info.version);
-    return json;
+    return json.records;
   } catch (ex) {
     console.log(ex);
 
