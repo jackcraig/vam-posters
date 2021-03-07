fda3a4a5af4faf18839add98e66d5f9fde00de8b
Jack Craig <jackthfc@gmail.com>
2021-03-07 18:36:22 +0000

    Added CSS file

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,12 @@
 module.exports = config => {
-  return {
-  	markdownTemplateEngine: 'njk',	// allows markdown files to be processed by Nunjucks
-    dataTemplateEngine: 'njk',	// allows data files to be processed by Nunjucks
-    htmlTemplateEngine: 'njk',	// allows html files to be processed by Nunjucks
-    dir: {
-      input: 'src',
-      output: 'dist'
-    }
-  };
+	config.addPassthroughCopy('src/main.css');
+	return {
+		markdownTemplateEngine: 'njk',	// allows markdown files to be processed by Nunjucks
+		dataTemplateEngine: 'njk',	// allows data files to be processed by Nunjucks
+		htmlTemplateEngine: 'njk',	// allows html files to be processed by Nunjucks
+		dir: {
+			input: 'src',
+			output: 'dist'
+			}
+		};
 };
