261ae69e0bd7d632e06af71f658fa08153aad0d5
Jack Craig <jackthfc@gmail.com>
2021-03-07 13:49:38 +0000

    Removed unnecessary &

--- src/_data/objs.js
+++ src/_data/objs.js
@@ -11,7 +11,7 @@ const Cache = require('@11ty/eleventy-cache-assets');
 module.exports = async function() {
   try {
     // https://developer.github.com/v3/repos/#get
-    let json = await Cache("https://api.vam.ac.uk/v2/objects/search?&id_category=THES252692&kw_object_type=Poster&&page_size=100&response_format=json&order_by=date&order_sort=desc&images_exist=1", {
+    let json = await Cache("https://api.vam.ac.uk/v2/objects/search?&id_category=THES252692&kw_object_type=Poster&page_size=100&response_format=json&order_by=date&order_sort=desc&images_exist=1", {
       duration: "1d", // 1 day
       type: "json", // also supports "text" or "buffer"
       maxSize: 100
