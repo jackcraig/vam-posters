46f95be1bfef8844feba6f50e5c96c1a8cbdb890
Jack Craig <jackthfc@gmail.com>
2021-03-29 09:11:36 +0100

    Function to remove duplicates

--- src/_data/objs2.js
+++ src/_data/objs2.js
@@ -28,9 +28,24 @@ module.exports = async function() {
     try {
         const array1 = await fetchData("https://api.vam.ac.uk/v2/objects/search?&id_category=THES252692&kw_object_type=Poster&page_size=100&response_format=json&order_by=date&order_sort=desc&images_exist=1");
         const array2 = await fetchData("https://api.vam.ac.uk/v2/objects/search?id_person=A7753&kw_object_type=Poster&page_size=100&response_format=json&order_by=date&order_sort=desc&images_exist=1");
-        const records = [...array1, ...array2];
+
+        // Merges records into one array and uses systemNumber to remove duplcaites
+        function mergeDedupe(array) {
+            var a = array.concat();
+                for(var i=0; i<a.length; ++i) {
+                    // console.log("yep" + a[i].systemNumber);
+                for(var j=i+1; j<a.length; ++j) {
+                    if(a[i].systemNumber === a[j].systemNumber)
+                        a.splice(j--, 1);
+            }
+        }
+            return a;
+        }
+
+        var records = mergeDedupe(array1.concat(array2));
+
        // return the promise for each project <Promise{ title: ... }>
-        console.log(records);
+        // console.log(records);
         return { records };
     } catch (e) {
         console.log("Error returning multiple projects cached API data");
